7181e0a3db2d07e805582cf3c69db28273a0f31a
adan1999 <55188140+adan1999@users.noreply.github.com>
2019-10-17 04:57:23 -0500

    Add files via upload

---
+++ d5_3/src/main/java/hadoop/ch05/l17124080208/App.java
@@ -0,0 +1,13 @@
+package hadoop.ch05.l17124080208;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
